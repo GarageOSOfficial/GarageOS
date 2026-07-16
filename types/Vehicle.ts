7fd5212ae791d8e6d93bac095b9f4a4189d31091
The Garage OS <swearlikeasailorllc@gmail.com>
2026-07-16 05:10:11 +0000

    Sprint 3 - Build Log complete

---
+++ types/Vehicle.ts
@@ -0,0 +1,10 @@
+export interface Vehicle {
+    id: string;
+    year: number;
+    make: string;
+    trim?: string;
+    VIN?: string;
+    mileage: number;
+    Nickname?: string;
+    image?: string;
+}
